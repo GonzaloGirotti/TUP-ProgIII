5e74cf641498a525e5b364bcac2d1b055b0d610a
Gonzalo Girotti <gongirotti2001@gmail.com>
2025-06-08 21:16:32 -0300

    final changes

--- PracticoIII/src/database/config/config.js
+++ PracticoIII/src/database/config/config.js
@@ -4,19 +4,5 @@ module.exports = {
   "development": {
     "host": "127.0.0.1",
     "dialect": "sqlite"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "sqlite"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "sqlite"
   }
 }
